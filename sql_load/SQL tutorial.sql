bdbe196643eefb3d70f62ddbbebe87a1ebae480b
kallt <50804704+kallt@users.noreply.github.com>
2024-05-01 11:01:56 +0200

    first commit

---
+++ sql_load/SQL tutorial.sql
@@ -0,0 +1,40 @@
+
+-- SubQuery
+SELECT *
+FROM (
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date ) = 1
+) AS january_jobs;
+
+
+--CTE
+WITH january_jobs AS (
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1
+)
+
+SELECT *
+FROM january_jobs;
+
+
+
+SELECT 
+    company_id,
+    name AS company_name
+FROM 
+    company_dim
+WHERE 
+    company_id IN (
+    SELECT
+    company_id
+    FROM
+    job_postings_fact
+    WHERE
+    job_no_degree_mention = TRUE
+    ORDER BY
+    company_id
+)
+
+
